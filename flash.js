038b2ab818ea676e6aa896cf03c4e289d3bbc4f1
renondt <101500715+renondt@users.noreply.github.com>
2022-03-13 20:34:23 +0900

    Add files via upload

---
+++ flash.js
@@ -0,0 +1,17 @@
+function mainflash(Str1, Str2, Str3){
+document.write('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="'+Str2+'" height="'+Str3+'" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" id=ShockwaveFlash1>'
++'<param name="movie" value="'+Str1+'">'
++'<param name="quality" value="high">'
++'<param name="wmode" value="transparent">'
++'<embed src="'+Str1+'" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" width="'+Str2+'" height="'+Str3+'" type="application/x-shockwave-flash"></embed>'
++'</object>');
+}
+
+function mainflash2(Str1, Str2, Str3){
+document.write('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="'+Str2+'" height="'+Str3+'" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" id=ShockwaveFlash1>'
++'<param name="movie" value="'+Str1+'">'
++'<param name="quality" value="high">'
++'<param name="wmode" value="transparent">'
++'<embed src="'+Str1+'" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" width="'+Str2+'" height="'+Str3+'" type="application/x-shockwave-flash"></embed>'
++'</object>');
+}
